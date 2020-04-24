eacfcd493b79962f797e1bd76418e19e9990d341
Rahul <rahulmukherjee68@gmail.com>
2020-04-24 21:44:08 +0530

    added date filter

--- controller/getController.js
+++ controller/getController.js
@@ -15,3 +15,37 @@ module.exports.get = async (link, skip, limit) => {
 
     }
 }
+module.exports.getByDate = async (link, start, end) => {
+    try {
+        start = new Date(start);
+        end = new Date(end)
+        console.log(start);
+        console.log(end);
+        let doc = "";
+        if (link == "global") {
+            console.log("global");
+
+            doc = await articleSchema.find({
+                lastmod: {
+                    $gt: start,
+                    $lt: end
+                }
+            });
+        }
+        else {
+            console.log("not global");
+
+            doc = await articleSchema.find({
+                main_link: link,
+                lastmod: {
+                    $gt: start,
+                    $lt: end
+                }
+            });
+        }
+        return (doc)
+    }
+    catch (err) {
+        return ({ status: false, result: null, err: err });
+    }
+}

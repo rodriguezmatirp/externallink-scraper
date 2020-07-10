fe02b5541108d7acde4e154d8d81348e20cf4923
Tarun Pritam R <52533898+rodriguezmatirp@users.noreply.github.com>
2020-07-10 22:03:12 +0530

    commit

--- model/master.js
+++ model/master.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MasterSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     link: { type: String, required: true, unique: true },
     algo: { type: String, required: true },
     created_at: { type: Date, required: false, default: Date.now() },
